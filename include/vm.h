8d5743b034f0ee9843362613dfbc0bd473755818
yjy <522432938@qq.com>
2022-03-15 14:37:14 +0800

    finish scanner

---
+++ include/vm.h
@@ -0,0 +1,29 @@
+#ifndef clox_vm_h
+#define clox_vm_h
+#include "compiler.h"
+#include "chunk.h"
+#include "value.h"
+#define STACK_MAX 256
+
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+typedef struct
+{
+    Chunk *chunk;
+    uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value *stackTop;
+} VM;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(const char* source);
+static InterpretResult run();
+void push(Value value);
+Value pop();
+#endif
